62e82b2fe43d9646ece658be5120b2d7dc0524ea
corvinus-tgpztt <78882137+corvinus-tgpztt@users.noreply.github.com>
2022-05-29 18:30:45 +0200

    Szimulált hűtés
    SA

---
+++ Szimulalt_hutes/Szimulalt_hutes/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿
+namespace Szimulalt_hutes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(863, 210);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Idő egy homersekleten";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Location = new System.Drawing.Point(866, 227);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(76, 20);
+            this.textBox2.TabIndex = 10;
+            this.textBox2.Text = "10";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(850, 133);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Indulási hőmérséklet";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(866, 150);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(76, 20);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.Text = "10000";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(852, 331);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 43);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Hűtés";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(44, 32);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(805, 342);
+            this.listBox1.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(852, 284);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 43);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Statisztika";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1004, 407);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Szimulalt_hutes/Szimulalt_hutes/Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MathNet.Numerics.Statistics;
+
+
+namespace Szimulalt_hutes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public struct Egyed
+        {
+            public double X1;
+            public double X2;
+            public double Fx;
+
+        }
+
+        double max = 10; // A maximum érték  abs értéke. (negatívan is rá fogjuk mérni)
+        double homerseklet =0;
+        double delta = 0;
+        double lehules = 0.9999;
+        double minhom = 0.00001; // Itt all le
+        double time=0; // ennyi ideig marad a homersekleten
+        List<double> eredmeny = new List<double>();
+        Random r = new Random();
+
+        Egyed regi = new Egyed();
+        Egyed uj = new Egyed();
+        
+        TimeSpan futásidő = DateTime.Now - DateTime.Now;
+        double vegeredmeny = 1.0316285;
+        DateTime Vége;
+        int iteracio;
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            DateTime Start = DateTime.Now;
+            homerseklet = Convert.ToDouble(textBox1.Text);
+            time = Convert.ToDouble(textBox2.Text);
+            int exit = 0;
+            double referencia = 0;
+           iteracio = 0;
+
+
+
+
+            
+            
+
+            //maga a tesztfüggvény
+
+            Func<double, double, double> fx2 = ((x, y) => -(4 * Math.Pow(x, 2) - 2.1 * Math.Pow(x, 4) + Math.Pow(x, 6) / 3 + x * y - 4 * Math.Pow(y, 2) + 4 * Math.Pow(y, 4)));
+           // Func<double, double, double> fx2 = ((x, y) => -100 * Math.Pow(y - (Math.Pow(x, 2)), 2) + Math.Pow(1 - x, 2));
+
+            //első megoldás generálása
+
+
+            regi.X1 = r.NextDouble() * max;
+            if (r.Next(2) == 0)
+            {
+                regi.X1 = regi.X1 * -1;
+            }
+
+            regi.X2 = r.NextDouble() * max;
+            if (r.Next(2) == 0)
+            {
+                regi.X2 = regi.X2 * -1;
+            }
+
+            // Fitness számítása - ez maga az Fx lesz. (szerintem)
+            regi.Fx = fx2(regi.X1, regi.X2);
+
+
+            
+            
+
+            while (homerseklet > minhom)
+            {
+               
+                for (int i = 0; i < time; i++)
+                {
+                    uj = regi;
+
+                    referencia = regi.Fx;
+
+
+                    //Módosulás
+
+                    //Első paraméter
+                    if (r.Next(2) == 1)
+                    {
+                        //növelem
+                        uj.X1 = uj.X1 + (max * r.NextDouble());
+                    }
+                    else
+                    {
+                        //növelem
+                        uj.X1 = uj.X1 - (max * r.NextDouble());
+                    }
+                    //Második paraméter
+                    if (r.Next(2) == 1)
+                    {
+                        //növelem
+                        uj.X2 = uj.X2 + (max * r.NextDouble());
+                    }
+                    else
+                    {
+                        //növelem
+                        uj.X2 = uj.X2 - (max * r.NextDouble());
+                    }
+
+                    uj.Fx = fx2(uj.X1, uj.X2);
+
+                    //Kiszámoljuk a differenciát az új és a régi között.
+
+                    delta = uj.Fx - regi.Fx;
+                    
+
+                    if (delta > 0)
+                    {
+                        //Közelebb kerültünk a maximumhoz.
+                        regi = uj;
+
+                    }                    
+                    else if (Math.Exp(-delta / homerseklet) < r.NextDouble())
+                    {
+                        regi = uj;
+                    }
+
+
+                     double diff = Math.Abs(referencia - regi.Fx);
+                     if ( diff < 0.01)
+                     {
+                         exit++;
+                     }
+                     else exit = 0;
+                    
+                     if (exit == 5000)
+                     {
+                         break;
+                     }
+                    
+
+                  
+                    iteracio++;
+
+                
+                }
+                if (exit == 5000) break;
+
+                
+                
+
+
+
+                
+                homerseklet = homerseklet * lehules;
+                time = time * lehules;
+              }
+             Vége = DateTime.Now;
+             futásidő = Vége - Start;
+
+
+
+
+            listBox1.Items.Add((iteracio).ToString() + " lépésből az optimum: X1= " + Math.Round(regi.X1, 4) + ", X2= " + Math.Round(regi.X2, 4) + " F(x): " + Math.Round(regi.Fx, 5) + ". " + (Math.Round((regi.Fx / vegeredmeny * 100), 5)) + "%-os az egyezőség az optimummal." + "Futásidő: "+ futásidő.Milliseconds + " milliszekundum");
+        
+    }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            var lst = new List<double> { 1,5,7,10,12,15,20,100,1000,10000 };
+            var pontos = new List<double>();
+            var lepes = new List<int>();
+            var ido = new List<double>();
+            using (StreamWriter sw = new StreamWriter("Output.csv"))
+            {
+                for (int i = 0; i < lst.Count; i++)
+                {
+
+                    textBox2.Text = lst[i].ToString();
+                    for (int j = 0; j < 100; j++)
+                    {
+                        button1.PerformClick();
+                        pontos.Add(Math.Round((regi.Fx / vegeredmeny * 100), 5));
+                        ido.Add(futásidő.Milliseconds);
+                        lepes.Add(iteracio);
+
+                    }
+                    sw.WriteLine(lst[i] + ";" + lepes.Average() + ";" + pontos.Average() + ";" + pontos.Median() + ";" + Math.Sqrt(pontos.Variance()) + ";" + ido.Average());
+                    pontos.Clear();
+                    ido.Clear();
+                    lepes.Clear();
+
+                }
+            }
+
+            /* using (StreamWriter sw = new StreamWriter("Output.csv"))
+            {
+                sw.WriteLine("Iteracio ; X1 ; X2 ; F(x) ; pontos egyezoseg; futas (milliszekundum)");
+                for (int i = 0; i < 30; i++)
+                {
+                    button1.PerformClick();
+                    sw.WriteLine(((iteracio).ToString() + ";" + Math.Round(regi.X1, 4) + ";" + Math.Round(regi.X2, 4) + ";" + Math.Round(regi.Fx, 5) + ";" + (Math.Round((regi.Fx / vegeredmeny * 100), 5)) + ";" + futásidő.Milliseconds));
+                    
+                }
+            }*/
+
+
+        }
+    }
+}
